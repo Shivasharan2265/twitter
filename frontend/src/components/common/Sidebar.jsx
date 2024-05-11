d7725d4f5a89da3a666da5d0d551f4849d852a3f
Shivasharan2265 <shivasharan2265@gmail.com>
2024-05-11 23:18:52 +0530

    ss

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
 
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
-      <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full">
+      <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-10 md:w-full ">
         <Link to="/" className="flex justify-center md:justify-start">
           <XSvg className="px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900" />
         </Link>
@@ -93,7 +93,7 @@ const Sidebar = () => {
                 <p className="text-slate-500 text-sm">@{authUser?.username}</p>
               </div>
               <BiLogOut
-                className="w-5 h-5 cursor-pointer"
+                className="w-4 h-5 cursor-pointer"
                 onClick={(e) => {
                   e.preventDefault();
                   logout();
